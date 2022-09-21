428cd80b115aeb028f2a4b929ba6a86831ad15ee
Junaid Ali <Junaid Ali>
2022-09-21 21:53:27 +0500

    headless updtae

--- members/views.py
+++ members/views.py
@@ -10,8 +10,17 @@ import json
 
 def index(request):
     options = Options()
-    # options.headless = True
+    options.headless = True
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     # Instantiate a webdriver
+    options.headless = True
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument(f'user-agent={user_agent}')
+
+    options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Chrome(options=options)
     # Load the HTML page
     link = request.GET.get(
@@ -20,6 +29,7 @@ def index(request):
     print(link)
     # Parse processed webpage with BeautifulSoup
     soup = BeautifulSoup(driver.page_source)
+    print(soup)
     # print(soup)
     driver.quit()
     # el = 'script', {'id': '__NEXT_DATA__', 'type': 'application/json'}
