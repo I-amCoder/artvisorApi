959b38ec66219e21b25e896a5d73430461e74891
Junaid Ali <Junaid Ali>
2022-09-22 06:25:01 +0500

    fake-useragent

--- members/views.py
+++ members/views.py
@@ -5,21 +5,26 @@ from django.http import JsonResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import json
 
 
 def index(request):
     options = Options()
     options.headless = True
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    ua = UserAgent()
+
+    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    user_agent = ua.random
     # Instantiate a webdriver
-    options.headless = True
+
     options.add_argument("start-maximized")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--no-sandbox')
     options.add_argument(f'user-agent={user_agent}')
-
+    options.headless = True
     options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Chrome(options=options)
     # Load the HTML page
