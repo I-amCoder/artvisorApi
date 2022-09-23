d30f0843983e30223f5424e062bb2dd58eccd8c6
Junaid Ali <86035380+I-amCoder@users.noreply.github.com>
2022-09-23 16:18:29 +0500

    update views

--- members/views.py
+++ members/views.py
@@ -20,6 +20,8 @@ def index(request):
 
     options.add_argument("start-maximized")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument('--no-sandbox')
