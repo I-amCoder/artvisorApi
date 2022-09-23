c6720535f32df9ad60c9d3f61ce908db11086046
Junaid Ali <Junaid Ali>
2022-09-23 18:25:35 +0500

    new view

--- members/views.py
+++ members/views.py
@@ -10,32 +10,21 @@ import json
 
 
 def index(request):
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+
     options = Options()
-    options.headless = True
-    ua = UserAgent()
-
-    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    user_agent = ua.random
-    # Instantiate a webdriver
-
-    options.add_argument("start-maximized")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
     options.add_argument(f'user-agent={user_agent}')
     options.headless = True
-    options.add_argument('--disable-blink-features=AutomationControlled')
+    # Instantiate a webdriver
     driver = webdriver.Chrome(options=options)
     # Load the HTML page
     link = request.GET.get(
         'url', 'https://art.co/search?query=artist&type=artworks')
     driver.get(link)
+
+    # Parse processed webpage with BeautifulSoup
     soup = BeautifulSoup(driver.page_source)
     driver.quit()
-    # el = 'script', {'id': '__NEXT_DATA__', 'type': 'application/json'}
     data = soup.find('script', {'id': '__NEXT_DATA__',
-                     'type': 'application/json'}).text
-    # print(json.dumps(data))
+                                'type': 'application/json'}).text
     return JsonResponse(json.loads(data))
