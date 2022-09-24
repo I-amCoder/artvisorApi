30fdf00afbb0a8df225a22fe01753f7b1246f5ac
Junaid Ali <Junaid Ali>
2022-09-24 14:36:15 +0500

    update

--- members/views.py
+++ members/views.py
@@ -1,4 +1,5 @@
 from urllib import request
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
@@ -13,7 +14,9 @@ def index(request):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
     options = Options()
+    # pr = '75.119.206.48:15745'
     options.add_argument(f'user-agent={user_agent}')
+    # options.add_argument('--proxy-server=%s' % pr)
     options.headless = True
 
     # Instantiate a webdriver
