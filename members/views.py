604bad0f5c6a7ebfde218e4e452e0402f83c87d1
Junaid Ali <Junaid Ali>
2022-09-25 10:13:28 +0500

    heroku-setup

--- members/views.py
+++ members/views.py
@@ -8,19 +8,27 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import json
+import os
 
 
 def index(request):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
-    options = Options()
-    # pr = '75.119.206.48:15745'
-    options.add_argument(f'user-agent={user_agent}')
-    # options.add_argument('--proxy-server=%s' % pr)
-    options.headless = True
+    chrome_options = Options()
+    # # pr = '75.119.206.48:15745'
+    # options.add_argument(f'user-agent={user_agent}')
+    # options.binary_location = os.environ.get('GOOGLE_CHROME_BIN ')
+    # # options.add_argument('--proxy-server=%s' % pr)
+    # options.headless = True
 
     # Instantiate a webdriver
-    driver = webdriver.Chrome(options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     # Load the HTML page
     link = request.GET.get(
