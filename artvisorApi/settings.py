2619405d8a62a09aaf5bd01934b660f714105600
Junaid Ali <Junaid Ali>
2022-09-25 14:58:07 +0500

    new vps

--- artvisorApi/settings.py
+++ artvisorApi/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -119,14 +117,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATIC_ROOT = os . path.join(BASE_DIR, ' staticfiles ')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, ' static '), )
-django_heroku.settings(locals())
--- members/views.py
+++ members/views.py
@@ -1,43 +1,42 @@
 from urllib import request
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 import json
-import os
 
 
 def index(request):
+    options = Options()
+    options.headless = True
+    # ua = UserAgent()
+
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-
-    chrome_options = Options()
-    # # pr = '75.119.206.48:15745'
-    # options.add_argument(f'user-agent={user_agent}')
-    # options.binary_location = os.environ.get('GOOGLE_CHROME_BIN ')
-    # # options.add_argument('--proxy-server=%s' % pr)
-    # options.headless = True
-
+    # user_agent = ua.random
     # Instantiate a webdriver
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get(
-        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument(f'user-agent={user_agent}')
+    options.headless = True
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    driver = webdriver.Chrome(options=options)
     # Load the HTML page
     link = request.GET.get(
         'url', 'https://art.co/search?query=artist&type=artworks')
     driver.get(link)
-
-    # Parse processed webpage with BeautifulSoup
     soup = BeautifulSoup(driver.page_source)
     driver.quit()
+    # el = 'script', {'id': '__NEXT_DATA__', 'type': 'application/json'}
     data = soup.find('script', {'id': '__NEXT_DATA__',
-                                'type': 'application/json'}).text
+                     'type': 'application/json'}).text
+    # print(json.dumps(data))
     return JsonResponse(json.loads(data))
